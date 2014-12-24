79658c09390bb3484028a6a0e0f1b1fda4646e02
Napfkuchen <sergeifladung@googlemail.com>
2014-12-24 21:55:51 +0100

    Added Tour_Category relationship

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,4 +1,6 @@
 class Tour < ActiveRecord::Base
-  validates :title, :teaser, :description, :url, presence: true
-  validates :url, format: %r|\Ahttp(s?)://www.google.com/maps/|
+has_many :tour_categories
+has_many :categories, through: :tour_categories
+validates :title, :teaser, :description, :url, presence: true
+validates :url, format: %r|\Ahttp(s?)://www.google.com/maps|
 end
